633b10b0ac053b39abecc6bae96beb7f90e3d2a1
Bestony <best.tony@foxmail.com>
2019-01-21 17:19:29 +0800

    feat: move config from app.js to config.js

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,18 +1,26 @@
 const mta = require('vendor/mta_analysis.js');
+const config = require('config.js');
 App({
   onLaunch: function (options) {
+    /**
+     * 初始化 MTA  检测
+     */
     mta.App.init({
-      "appID": "500668228",
-      "eventID": "500668246",
+      "appID": config.mtaAppID,
+      "eventID": config.mtaEventID,
       "statShareApp": true,
       "statReachBottom": true,
       "lauchOpts": options,
     });
+    /**
+     * 初始化云开发
+     */
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
       wx.cloud.init({
         traceUser: true,
+        env: config.envID
       })
     }
 
---
+++ miniprogram/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  "mtaAppID": "500668228",
+  "mtaEventID":"500668246",
+  "envID":"demo-3ee737"
+}
