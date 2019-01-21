9f2a140901462f3554cc3bac2b529d5077f41e55
Bestony <best.tony@foxmail.com>
2019-01-21 21:41:27 +0800

    feat: compete list page query data & compete reachBottom

--- miniprogram/pages/list/list.js
+++ miniprogram/pages/list/list.js
@@ -1,66 +1,46 @@
-// pages/list/list.js
+const mta = require('../../vendor/mta_analysis.js');
+const app = getApp();
+const db = wx.cloud.database()
+const store = db.collection('store');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    numbers: 0,
+    stores: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    mta.Page.init();
+    this.loadData();
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  loadData: function() {
+    store.skip(this.data.numbers).get().then(res => {
+      /**
+       * 如果没有数据，就提示没有商户了，并返回。
+       */
+      if (res.data.length == 0) {
+        wx.showToast({
+          title: '没有别的店铺了！',
+          icon: 'none'
+        });
+        return;
+      }
+      this.setData({
+        stores: this.data.stores.concat(res.data),
+        numbers: this.data.numbers + res.data.length
+      });
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onReachBottom: function() {
+    this.loadData();
   }
 })
