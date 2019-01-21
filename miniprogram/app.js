5feb28eafc14b9092eecc562fa8d19495521f5d9
Bestony <best.tony@foxmail.com>
2019-01-21 17:13:20 +0800

    feat: add mta analysis

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,7 +1,13 @@
-
+const mta = require('vendor/mta_analysis.js');
 App({
-  onLaunch: function () {
-    
+  onLaunch: function (options) {
+    mta.App.init({
+      "appID": "500668228",
+      "eventID": "500668246",
+      "statShareApp": true,
+      "statReachBottom": true,
+      "lauchOpts": options,
+    });
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
--- miniprogram/pages/map/map.js
+++ miniprogram/pages/map/map.js
@@ -1,4 +1,4 @@
-// pages/map/map.js
+const mta = require('../../vendor/mta_analysis.js');
 Page({
 
   /**
@@ -12,7 +12,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    mta.Page.init();
   },
 
   /**
