13ed942ea395280c54d5dbbba215c3f3dc73ce42
Bestony <best.tony@foxmail.com>
2019-01-21 20:59:01 +0800

    feat: create list && navigate to list page

--- miniprogram/pages/map/map.js
+++ miniprogram/pages/map/map.js
@@ -52,35 +52,11 @@ Page({
     })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
-
+  viewAll:function(){
+    wx.navigateTo({
+      url: '../list/list',
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-
   /**
    * 用户点击右上角分享
    */
