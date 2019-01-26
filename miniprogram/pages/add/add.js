347a71a27c4fca4beea007d3d26a6007fd08ffa2
Bestony <best.tony@foxmail.com>
2019-01-26 18:06:25 +0800

    feat: refactor upload by promise

--- miniprogram/pages/add/add.js
+++ miniprogram/pages/add/add.js
@@ -17,32 +17,32 @@ Page({
   onLoad: function (options) {
     mta.Page.init();
   },
-  chooseLocation:function(event){
+  chooseLocation: function (event) {
     wx.chooseLocation({
-      success:res => {
+      success: res => {
         this.setData({
           address: res.address,
           latitude: res.latitude,
           longitude: res.longitude,
-          name:res.name
+          name: res.name
         })
       }
     })
   },
-  createItem:function(event){
+  createItem: function (event) {
     wx.showLoading({
       title: '上传数据中...',
     })
     let value = event.detail.value
     store.add({
-      data:{
+      data: {
         ...value,
-        thumbs_up:1,
-        iconPath:"/images/food.png",
+        thumbs_up: 1,
+        iconPath: "/images/food.png",
         longitude: this.data.longitude,
         latitude: this.data.latitude,
-        label:{
-          content:value.title
+        label: {
+          content: value.title
         },
         images: this.data.images
       }
@@ -50,8 +50,8 @@ Page({
       wx.hideLoading();
       wx.showToast({
         title: '创建成功！',
-        icon:'success',
-        success:res => {
+        icon: 'success',
+        success: res => {
           wx.navigateBack({
           })
         }
@@ -60,30 +60,48 @@ Page({
       console.error(error);
     })
   },
-  uploadImage:function(e){
+  uploadImage: function (e) {
     wx.chooseImage({
-      count:9,
+      count: 9,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
       success: res => {
-        let img_array = [];
-        for(let i = 0;i < res.tempFilePaths.length; i++){
-          wx.showLoading({
-            title: parseInt(i+1) + '/' + res.tempFilePaths.length,
-          })
-          wx.cloud.uploadFile({
-            cloudPath: `${Date.now()}-${Math.floor(Math.random(0, 1) * 10000000)}.png`,
-            filePath: res.tempFilePaths[i]
-          }).then(res => {
-            img_array.push(res.fileID);
-            this.setData({
-              images: img_array
-            },res => {
-              wx.showToast({
-                title: '图片上传完成',
-              })
-            })
+
+        wx.showLoading({ title: '上传中' })
+        let tempFilePaths = res.tempFilePaths
+        let items = [];
+        for (const tempFilePath of tempFilePaths) {
+          items.push({
+            src: tempFilePath
           })
         }
+        const uploadTask = items.map(item => this.uploadPhoto(item.src))
+
+        Promise.all(uploadTask).then(result => {
+
+          let urls = [];
+          for (const file of result) {
+            urls.push(file.fileID);
+          }
+          this.setData({
+            images: urls
+          }, res => {
+            wx.hideLoading();
+            wx.showToast({ title: '上传图片成功', icon: 'success' })
+          })
+        }).catch(() => {
+          wx.hideLoading()
+          wx.showToast({ title: '上传图片错误', icon: 'error' })
+        })
+
+        this.setData({ tempPhoto: items })
       }
+    })
+  },
+  uploadPhoto(filePath) {
+    return wx.cloud.uploadFile({
+      cloudPath: `${Date.now()}-${Math.floor(Math.random(0, 1) * 10000000)}.png`,
+      filePath
     })
   }
 })
