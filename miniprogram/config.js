753d82507ed9c26f522f1d80791b6daed2d85818
Bestony <best.tony@foxmail.com>
2019-01-27 10:54:56 +0800

    config: update config for production

--- miniprogram/config.js
+++ miniprogram/config.js
@@ -4,6 +4,6 @@ module.exports = {
   "mtaEventID":"500668246",
   "envID":"demo-3ee737",
   "mapSubKey":"URTBZ-FLY64-OVLUX-XHH4A-5DOGO-YKFFP",
-  "center_longitude": 113.22,
-  "center_latitude": 35.25,
+  "center_longitude": 113.921736,
+  "center_latitude": 22.538017,
 }
