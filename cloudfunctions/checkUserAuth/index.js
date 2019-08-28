06264af65bbc5d4194de0f6dd38c163f565b47b4
Bestony <best.tony@foxmail.com>
2019-08-28 18:29:13 +0800

    feat:task 13

--- cloudfunctions/checkUserAuth/index.js
+++ cloudfunctions/checkUserAuth/index.js
@@ -8,18 +8,24 @@ exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()  
   const administrator = process.env.ADMIN.split('|');
 
-  if (administrator.indexOf(wxContext.OPENID) == -1){
-    return {
-      data:{
-        is_administrator:false
-      }
-    }
-  }else{
-    return {
-      data: {
-        is_administrator: true
-      }
-    }
-  }
+  /**
+   * @task #13 添加管理员身份校验函数 ID
+   * @chapter 1.5.10.2  添加管理员身份校验函数 ID
+   * 请取消下方代码的注释
+   */
+
+  // if (administrator.indexOf(wxContext.OPENID) == -1){
+  //   return {
+  //     data:{
+  //       is_administrator:false
+  //     }
+  //   }
+  // }else{
+  //   return {
+  //     data: {
+  //       is_administrator: true
+  //     }
+  //   }
+  // }
 
 }
