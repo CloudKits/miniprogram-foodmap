781243d69a44acb979e8befeea253b5fac12f3d3
Bestony <best.tony@foxmail.com>
2019-08-28 18:28:25 +0800

    task#12

--- cloudfunctions/getUserOpenId/index.js
+++ cloudfunctions/getUserOpenId/index.js
@@ -5,9 +5,16 @@ cloud.init()
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
 
-  return {
-    openid: wxContext.OPENID
-  }
+  /**
+   * @task #12 获取用户身份 ID
+   * @chapter 1.5.10.1  获取用户身份 ID
+   * 请取消下方代码的注释
+   */
+
+  // const wxContext = cloud.getWXContext()
+
+  // return {
+  //   openid: wxContext.OPENID
+  // }
 }
