150f9892fe4646e5b64ba24b712bb1affbd4c00d
Bestony <best.tony@foxmail.com>
2019-08-28 18:20:59 +0800

    feat: #5

--- miniprogram/pages/list/list.js
+++ miniprogram/pages/list/list.js
@@ -18,22 +18,27 @@ Page({
     this.loadData();
   },
   loadData: function() {
-    store.skip(this.data.numbers).get().then(res => {
-      /**
-       * 如果没有数据，就提示没有商户了，并返回。
-       */
-      if (res.data.length == 0) {
-        wx.showToast({
-          title: '没有别的店铺了！',
-          icon: 'none'
-        });
-        return;
-      }
-      this.setData({
-        stores: this.data.stores.concat(res.data),
-        numbers: this.data.numbers + res.data.length
-      });
-    })
+
+    /**
+     * @task #5 开发列表功能
+     * @chapter 1.5.6  开发列表功能
+     * 请取消下方代码的注释
+     */
+
+    // store.skip(this.data.numbers).get().then(res => {
+    //   if (res.data.length == 0) {
+    //     wx.showToast({
+    //       title: '没有别的店铺了！',
+    //       icon: 'none'
+    //     });
+    //     return;
+    //   }
+    //   this.setData({
+    //     stores: this.data.stores.concat(res.data),
+    //     numbers: this.data.numbers + res.data.length
+    //   });
+    // })
+
   },
   /**
    * 页面上拉触底事件的处理函数
