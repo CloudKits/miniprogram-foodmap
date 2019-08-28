48d9b566128a35c0936e46e55e2bc3f403d58b75
Bestony <best.tony@foxmail.com>
2019-08-28 18:16:02 +0800

    feat: task#1

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -7,10 +7,19 @@ App({
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
-      wx.cloud.init({
-        traceUser: true,
-        env: config.envID
-      })
+      /**
+       * @task #1 初始化云开发能力
+       * @chapter 1.5.2  初始化云开发能力
+       * 请取消下方代码的注释
+       */
+
+
+      // wx.cloud.init({
+      //   traceUser: true,
+      //   env: config.envID
+      // })
+
+
     }
     /**
      * 获取屏幕高度
