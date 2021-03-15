3d4a0d540022760a083cea9f88ea16ca233ed038
Volcano-Yang <648941183@qq.com>
2021-03-15 10:14:46 +0800

    feat(mta): remove mta

--- miniprogram/config.js
+++ miniprogram/config.js
@@ -1,7 +1,9 @@
+/**
+ * envID: 云开发环境id
+ * mapSubKey： 腾讯地图key
+ */
 module.exports = {
   "appName":"深圳美食图鉴",
-  "mtaAppID": "500668228",
-  "mtaEventID":"500668246",
   "envID":"demo-3ee737",
   "mapSubKey":"URTBZ-FLY64-OVLUX-XHH4A-5DOGO-YKFFP",
   "center_longitude": 113.921736,
--- miniprogram/pages/list/list.js
+++ miniprogram/pages/list/list.js
@@ -1,4 +1,3 @@
-const mta = require('../../vendor/mta_analysis.js');
 const app = getApp();
 const db = wx.cloud.database()
 const store = db.collection('store');
@@ -16,8 +15,6 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-   
-    mta.Page.init();
     this.loadData();
   },
   loadData: function() {
