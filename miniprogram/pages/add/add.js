599eef225da73af2adbe0f7cfae671eb3652e8f4
Volcano-Yang <648941183@qq.com>
2021-03-16 18:27:41 +0800

    feat(add): change creatItem

--- miniprogram/pages/add/add.js
+++ miniprogram/pages/add/add.js
@@ -1,129 +1,135 @@
 const app = getApp();
-const db = wx.cloud.database()
-const store = db.collection('store');
+const db = wx.cloud.database();
+const store = db.collection("store");
 Page({
-
   /**
    * 页面的初始数据
    */
-  data: {
-
-  },
+  data: {},
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function (options) {},
 
-  },
   chooseLocation: function (event) {
     wx.getSetting({
-      success: res => {
-        if (!res.authSetting['scope.userLocation']){
+      success: (res) => {
+        if (!res.authSetting["scope.userLocation"]) {
           wx.authorize({
-            scope: 'scope.userLocation',
-            success:res => {
+            scope: "scope.userLocation",
+            success: (res) => {
               wx.chooseLocation({
-                success: res => {
+                success: (res) => {
                   this.setData({
                     address: res.address,
                     latitude: res.latitude,
                     longitude: res.longitude,
-                    name: res.name
-                  })
-                }
-              })
-            }
-          })
-        }else{
+                    name: res.name,
+                  });
+                },
+              });
+            },
+          });
+        } else {
           wx.chooseLocation({
-            success: res => {
+            success: (res) => {
               this.setData({
                 address: res.address,
                 latitude: res.latitude,
                 longitude: res.longitude,
-                name: res.name
-              })
-            }
-          })
+                name: res.name,
+              });
+            },
+          });
         }
-      }
-    })
-    
+      },
+    });
   },
+
   createItem: function (event) {
     wx.showLoading({
-      title: '上传数据中...',
-    })
-    let value = event.detail.value
-    store.add({
-      data: {
-        ...value,
-        thumbs_up: 1,
-        iconPath: "/images/food.png",
-        longitude: this.data.longitude,
-        latitude: this.data.latitude,
-        label: {
-          content: value.title
+      title: "上传数据中...",
+    });
+    let value = event.detail.value;
+    let label = value.title;
+    let iconPath = "/images/marker/error.png";
+    if (label === "盲道占用") iconPath = "/images/marker/occupy.png";
+    if (label === "盲道设计问题") iconPath = "/images/marker/design.png";
+
+    store
+      .add({
+        data: {
+          ...value,
+          thumbs_up: 1,
+          longitude: this.data.longitude,
+          latitude: this.data.latitude,
+          label,
+          iconPath,
+          images: this.data.images,
         },
-        images: this.data.images
-      }
-    }).then(res => {
-      wx.hideLoading();
-      wx.showToast({
-        title: '创建成功！',
-        icon: 'success',
-        success: res => {
-          wx.navigateBack({
-          })
-        }
       })
-    }).catch(error => {
-      console.error(error);
-    })
+      .then((res) => {
+        wx.hideLoading();
+        wx.showToast({
+          title: "创建成功！",
+          icon: "success",
+          success: (res) => {
+            wx.navigateBack({});
+          },
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   },
   uploadImage: function (e) {
     wx.chooseImage({
       count: 9,
-      sizeType: ['original', 'compressed'],
-      sourceType: ['album', 'camera'],
-      success: res => {
-
-        wx.showLoading({ title: '上传中' })
-        let tempFilePaths = res.tempFilePaths
+      sizeType: ["original", "compressed"],
+      sourceType: ["album", "camera"],
+      success: (res) => {
+        wx.showLoading({ title: "上传中" });
+        let tempFilePaths = res.tempFilePaths;
         let items = [];
         for (const tempFilePath of tempFilePaths) {
           items.push({
-            src: tempFilePath
-          })
+            src: tempFilePath,
+          });
         }
-        const uploadTask = items.map(item => this.uploadPhoto(item.src))
+        const uploadTask = items.map((item) => this.uploadPhoto(item.src));
 
-        Promise.all(uploadTask).then(result => {
-
-          let urls = [];
-          for (const file of result) {
-            urls.push(file.fileID);
-          }
-          this.setData({
-            images: urls
-          }, res => {
-            wx.hideLoading();
-            wx.showToast({ title: '上传图片成功', icon: 'success' })
+        Promise.all(uploadTask)
+          .then((result) => {
+            let urls = [];
+            for (const file of result) {
+              urls.push(file.fileID);
+            }
+            this.setData(
+              {
+                images: urls,
+              },
+              (res) => {
+                wx.hideLoading();
+                wx.showToast({ title: "上传图片成功", icon: "success" });
+              }
+            );
           })
-        }).catch(() => {
-          wx.hideLoading()
-          wx.showToast({ title: '上传图片错误', icon: 'error' })
-        })
+          .catch(() => {
+            wx.hideLoading();
+            wx.showToast({ title: "上传图片错误", icon: "error" });
+          });
 
-        this.setData({ tempPhoto: items })
-      }
-    })
+        this.setData({ tempPhoto: items });
+      },
+    });
   },
   uploadPhoto(filePath) {
     return wx.cloud.uploadFile({
-      cloudPath: `${Date.now()}-${Math.floor(Math.random(0, 1) * 10000000)}.png`,
-      filePath
-    })
-  }
-})
+      cloudPath: `${Date.now()}-${Math.floor(
+        Math.random(0, 1) * 10000000
+      )}.png`,
+      filePath,
+    });
+  },
+});
--- miniprogram/pages/map/map.js
+++ miniprogram/pages/map/map.js
@@ -1,12 +1,10 @@
 const app = getApp();
-const config = require('../../config.js');
-const db = wx.cloud.database()
-const store = db.collection('store');
-const userInfo = db.collection('userInfo');
-
+const config = require("../../config.js");
+const db = wx.cloud.database();
+const store = db.collection("store");
+const userInfo = db.collection("userInfo");
 
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -23,7 +21,6 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
     let showAdmin = config.show_admin ? true : false;
 
     if (app.globalData.showAdmin) {
@@ -31,100 +28,105 @@ Page({
     }
 
     wx.showLoading({
-      title: '数据加载中...',
-    })
-    store.get().then(res => {
+      title: "数据加载中...",
+    });
+    store.get().then((res) => {
       let data = res.data;
       // 将 _id 给 id ,确保 marker 事件的正确触发
-      data.map(item => {
-        item.id = item._id
+      data.map((item) => {
+        item.id = item._id;
       });
-      this.setData({
-        stores: res.data,
-        windowHeight: app.globalData.windowHeight,
-        hideMe: false,
-        showAdmin: showAdmin,
-        defaultScale: config.default_scale
-      }, () => {
-        wx.hideLoading();
-        wx.showToast({
-          title: '双指缩放可以调整地图可视区域，查看更多美食',
-          icon: 'none'
-        })
-      })
-    })
+      this.setData(
+        {
+          stores: res.data,
+          windowHeight: app.globalData.windowHeight,
+          hideMe: false,
+          showAdmin: showAdmin,
+          defaultScale: config.default_scale,
+        },
+        () => {
+          wx.hideLoading();
+          wx.showToast({
+            title: "双指缩放可以调整地图可视区域，查看更多美食",
+            icon: "none",
+          });
+        }
+      );
+    });
 
-    this.getOpenID()
-
-    this.mapCtx = wx.createMapContext('map');
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    this.getOpenID();
+    this.mapCtx = wx.createMapContext("map");
     this.getCenterLocation();
   },
 
   onShow: function () {
     // #10 添加完成后更新一下 map
-    store.get().then(res => {
+    store.get().then((res) => {
       let data = res.data;
-      data.map(item => {
-        item.id = item._id
+      data.map((item) => {
+        item.id = item._id;
       });
       this.setData({
-        stores: res.data
-      })
-    })
+        stores: res.data,
+      });
+    });
   },
 
   viewAll: function () {
     wx.navigateTo({
-      url: '../list/list',
-    })
+      url: "../list/list",
+    });
   },
+
+  addMarker: function () {
+    wx.navigateTo({
+      url: "../add/add",
+    });
+  },
+  
   getUserInfo: function (e) {
     if (e.detail.userInfo) {
-      wx.cloud.callFunction({
-        name: 'checkUserAuth'
-      }).then(res => {
-        if (res.result.data.is_administrator) {
-          app.globalData.is_administrator = true;
-          wx.showModal({
-            title: '管理员登陆成功',
-            content: '管理员您好，是否要进入新增界面？',
-            success: res => {
-              if (res.cancel == false && res.confirm == true) {
-                wx.navigateTo({
-                  url: '../add/add',
-                })
-              } else {
-                wx.showToast({
-                  title: '您可以点击下方查看全部按钮管理已有数据',
-                  icon: 'none'
-                });
-              }
-            }
-          })
-        } else {
-          wx.showToast({
-            title: '您不是管理员，无法进入管理入口！',
-            icon: 'none'
-          });
-        }
-      })
+      wx.cloud
+        .callFunction({
+          name: "checkUserAuth",
+        })
+        .then((res) => {
+          if (res.result.data.is_administrator) {
+            app.globalData.is_administrator = true;
+            wx.showModal({
+              title: "管理员登陆成功",
+              content: "管理员您好，是否要进入新增界面？",
+              success: (res) => {
+                if (res.cancel == false && res.confirm == true) {
+                  wx.navigateTo({
+                    url: "../add/add",
+                  });
+                } else {
+                  wx.showToast({
+                    title: "您可以点击下方查看全部按钮管理已有数据",
+                    icon: "none",
+                  });
+                }
+              },
+            });
+          } else {
+            wx.showToast({
+              title: "您不是管理员，无法进入管理入口！",
+              icon: "none",
+            });
+          }
+        });
     } else {
       // 处理未授权的场景
       wx.showModal({
-        title: '授权失败',
-        content: '您尚未授权获取您的用户信息，是否开启授权界面？',
-        success: res => {
+        title: "授权失败",
+        content: "您尚未授权获取您的用户信息，是否开启授权界面？",
+        success: (res) => {
           if (res.confirm) {
-            wx.openSetting({})
+            wx.openSetting({});
           }
-        }
-      })
+        },
+      });
     }
   },
   /**
@@ -133,59 +135,61 @@ Page({
   getCenterLocation: function () {
     this.mapCtx.getCenterLocation({
       success: (res) => {
-        this.longitude=res.longitude;
-        this.latitude=res.latitude;
+        this.longitude = res.longitude;
+        this.latitude = res.latitude;
         console.log("当前中心点的位置：", this.longitude, this.latitude);
-      }
-    })
+      },
+    });
   },
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
     return {
-      title: '我在' + config.appName + '上发现了好吃的，你也看看吧！',
-      path: '/pages/map/map',
-      imageUrl: "/images/share.jpg"
-    }
+      title: "我在" + config.appName + "上发现了好吃的，你也看看吧！",
+      path: "/pages/map/map",
+      imageUrl: "/images/share.jpg",
+    };
   },
   onMarkerTap: function (event) {
     wx.navigateTo({
-      url: '../info/info?id=' + event.markerId,
-    })
+      url: "../info/info?id=" + event.markerId,
+    });
   },
   getOpenID: function (event) {
-    wx.cloud.callFunction({
-      name: "getUserOpenId"
-    }).then(res => {
-      console.log('openID已复制', res)
-      // wx.setClipboardData({
-      //   data: res.result.openid,
-      //   success: res => {
-      //     wx.showToast({
-      //       title: 'openID已复制',
-      //     })
-      //   }
-      // })
-    })
+    wx.cloud
+      .callFunction({
+        name: "getUserOpenId",
+      })
+      .then((res) => {
+        console.log("openID已复制", res);
+        // wx.setClipboardData({
+        //   data: res.result.openid,
+        //   success: res => {
+        //     wx.showToast({
+        //       title: 'openID已复制',
+        //     })
+        //   }
+        // })
+      });
   },
   hideMe: function (res) {
     this.setData({
-      hideMe: true
-    })
+      hideMe: true,
+    });
   },
   showAdmin: function (res) {
     wx.setStorage({
-      key: 'showAdmin',
+      key: "showAdmin",
       data: !this.data.showAdmin,
-    })
+    });
     this.setData({
-      showAdmin: !this.data.showAdmin
-    })
+      showAdmin: !this.data.showAdmin,
+    });
   },
   search: function () {
     wx.navigateTo({
-      url: '../search/search',
-    })
-  }
-})
+      url: "../search/search",
+    });
+  },
+});
