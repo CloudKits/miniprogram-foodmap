5cbeb1bae196b3c084c42a4ec854f11bd2c97d13
Volcano-Yang <648941183@qq.com>
2021-03-20 13:54:29 +0800

    feat(myList): add myList page ui

---
+++ miniprogram/pages/myList/myList.js
@@ -0,0 +1,66 @@
+// miniprogram/pages/myList/myList.js
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    isEmpty: true,
+    images: [
+      {
+        id: 1,
+        url:
+          "https://6d61-map-4g0ciu1x80002ab0-1305236624.tcb.qcloud.la/1615864269517-9804034.png?sign=7f69a65663712ca6ff6ccd920fcc8c7f&t=1616217565",
+      },
+      {
+        id: 2,
+        url:
+          "https://6d61-map-4g0ciu1x80002ab0-1305236624.tcb.qcloud.la/1615864269517-9804034.png?sign=7f69a65663712ca6ff6ccd920fcc8c7f&t=1616217565",
+      },
+      {
+        id: 3,
+        url:
+          "https://6d61-map-4g0ciu1x80002ab0-1305236624.tcb.qcloud.la/1615864269517-9804034.png?sign=7f69a65663712ca6ff6ccd920fcc8c7f&t=1616217565",
+      },
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {},
+
+  /*
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {},
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {},
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {},
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {},
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {},
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {},
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {},
+});
